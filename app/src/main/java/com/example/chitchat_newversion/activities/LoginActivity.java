3acf78cf56f4eeba575a299c70c34c9ba24d518d
Yu tianqi <15545567555@163.com>
2021-10-11 19:04:17 +0800

    prototype2.2
    the second version prototype of chitchat
    largely implement text-communication between users
    (bug: too much blank between texts)

--- app/src/main/java/com/example/chitchat_newversion/activities/LoginActivity.java
+++ app/src/main/java/com/example/chitchat_newversion/activities/LoginActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.example.chitchat_newversion.R;
 import com.example.chitchat_newversion.databinding.ActivityLoginBinding;
+import com.example.chitchat_newversion.utilities.Constants;
+import com.example.chitchat_newversion.utilities.PreferenceManger;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -16,13 +24,22 @@ public class LoginActivity extends AppCompatActivity {
 
     // enable viewBinding for this project
     private ActivityLoginBinding binding;
-
+    private PreferenceManger preferenceManger;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        preferenceManger = new PreferenceManger(getApplicationContext());
+
+        // sign in status
+        if(preferenceManger.getBoolean(Constants.KEY_IS_LOGINED))
+        {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
@@ -31,28 +48,119 @@ public class LoginActivity extends AppCompatActivity {
     private void setListeners()
     {
         binding.textRegister.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
+        binding.LoginButton.setOnClickListener(v -> {
+            if(isValidLoginDetails())
+            {
+                login();
+            }
+        });
+//        binding.LoginButton.setOnClickListener(v -> addDataToFirestore());
 
-        binding.LoginButton.setOnClickListener(v -> addDataToFirestore());
+        binding.checkbox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    binding.inputPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else
+                {
+                    binding.inputPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+
+    private void login()
+    {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() > 0)
+                    {
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManger.putBoolean(Constants.KEY_IS_LOGINED, true);
+                        preferenceManger.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManger.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManger.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        loading(false);
+                        showToast("can't login successfully");
+                    }
+                });
+
+    }
+
+    private void loading(Boolean isLoading)
+    {
+        if(isLoading)
+        {
+            binding.LoginButton.setVisibility((View.INVISIBLE));
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.LoginButton.setVisibility((View.VISIBLE));
+
+        }
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidLoginDetails()
+    {
+        if(binding.inputEmail.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input your email");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches())
+        {
+            showToast("You have to input correct email!");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input password");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
 
     // test code, to verify firebase database is set successful or not
 
-    private void addDataToFirestore()
-    {
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("first name", "yu");
-        data.put("last name", "tianqi");
-        database.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exception -> {
-                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
-
-                });
-    }
+//    private void addDataToFirestore()
+//    {
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("first name", "yu");
+//        data.put("last name", "tianqi");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception -> {
+//                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//                });
+//    }
 
 }
--- app/src/main/java/com/example/chitchat_newversion/activities/RegisterActivity.java
+++ app/src/main/java/com/example/chitchat_newversion/activities/RegisterActivity.java
@@ -1,29 +1,242 @@
 package com.example.chitchat_newversion.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.chitchat_newversion.R;
 import com.example.chitchat_newversion.databinding.ActivityLoginBinding;
 import com.example.chitchat_newversion.databinding.ActivityRegisterBinding;
+import com.example.chitchat_newversion.utilities.Constants;
+import com.example.chitchat_newversion.utilities.PreferenceManger;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
+    private PreferenceManger preferenceManger;
+    private String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        preferenceManger = new PreferenceManger(getApplicationContext());
+
         setListeners();
     }
 
     private void setListeners()
     {
         binding.textLogin.setOnClickListener(v -> onBackPressed());
+        binding.RegisterButton.setOnClickListener(v -> {
+            if(isValidRegisterDetails())
+            {
+                register();
+            }
+        });
+        binding.layoutImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            selectImage.launch(intent);
+        });
+
+        binding.checkbox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    binding.inputPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    binding.inputPasswordAgain.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else
+                {
+                    binding.inputPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    binding.inputPasswordAgain.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+    }
+
+    private void showToast(String message)
+    {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void register()
+    {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_NAME, binding.inputName.getText().toString());
+        user.put(Constants.KEY_EMAIL, binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE, encodedImage);
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    preferenceManger.putBoolean(Constants.KEY_IS_LOGINED, true);
+                    preferenceManger.putString(Constants.KEY_USER_ID, documentReference.getId());
+                    preferenceManger.putString(Constants.KEY_NAME, binding.inputName.getText().toString());
+                    preferenceManger.putString(Constants.KEY_IMAGE, encodedImage);
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                }).addOnFailureListener(exception ->{
+                    loading(false);
+                    showToast(exception.getMessage());
+        });
+    }
+
+    // read the image from phone
+    private String encodedImage(Bitmap bitmap)
+    {
+        int width = 150;
+        int height = bitmap.getHeight() * width / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, width, height,false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> selectImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK)
+                {
+                    if(result.getData() != null)
+                    {
+                        Uri imageUri = result.getData().getData();
+                        try
+                        {
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.userImage.setImageBitmap(bitmap);
+                            binding.textAddImage.setVisibility(View.GONE);
+                            encodedImage = encodedImage(bitmap);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+
+    );
+
+    private Boolean isValidRegisterDetails()
+    {
+        if(encodedImage == null)
+        {
+            showToast("Please select one profile image!");
+            return false;
+        }
+        else if(binding.inputName.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input your username!");
+            return false;
+        }
+        else if(!isNameRegax(binding.inputName.getText().toString()))
+        {
+            showToast("The total number of characters of username is limited to 6-10, it must start with a letter, and allow letters, digits and underscores");
+            return false;
+        }
+        else if(binding.inputEmail.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input email!");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches())
+        {
+            showToast("Your email format is not correct!!");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input password!");
+            return false;
+        }
+        else if(!isPasswordRegax(binding.inputPassword.getText().toString()))
+        {
+            showToast("The total number of characters of password is limited to 6-20, only allows letters and digits");
+            return false;
+        }
+        else if(binding.inputPasswordAgain.getText().toString().trim().isEmpty())
+        {
+            showToast("Please input password again!");
+            return false;
+        }
+        else if(!binding.inputPassword.getText().toString().equals(binding.inputPasswordAgain.getText().toString()))
+        {
+            showToast("Passwords you entered twice should be the same");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    // verify the format of username
+    // the total number of characters is limited to 6-10, it must start with a letter
+    // and allow letters, digits and underscores
+
+    private Boolean isNameRegax(String name)
+    {
+        String namePattern = "^[a-zA-Z][\\w_]{5,9}$";
+        return Pattern.matches(namePattern, name);
+    }
+
+    // verify the format of password
+    // the total number of characters is limited to 6-20, only allows letters and digits
+    private Boolean isPasswordRegax(String password)
+    {
+        String passwordPattern = "^[\\w]{6,20}$";
+        return Pattern.matches(passwordPattern, password);
+    }
+
+    private void loading(Boolean isLoading)
+    {
+        if(isLoading)
+        {
+            binding.RegisterButton.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.RegisterButton.setVisibility(View.VISIBLE);
+        }
     }
 
 }
