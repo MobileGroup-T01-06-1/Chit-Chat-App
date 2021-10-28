5d1cfc10460af626a165d4eefe6803cac1c9870b
Yu tianqi <15545567555@163.com>
2021-10-28 18:36:34 +0800

    bug fix: send null string

--- app/src/main/java/com/example/chitchat_newversion/activities/ChatActivity.java
+++ app/src/main/java/com/example/chitchat_newversion/activities/ChatActivity.java
@@ -101,30 +101,35 @@ public class ChatActivity extends AppCompatActivity {
 
     private void sendMessage()
     {
-        HashMap<String, Object> message = new HashMap<>();
-        message.put(Constants.KEY_SENDER_ID, preferenceManger.getString(Constants.KEY_USER_ID));
-        message.put(Constants.KEY_RECEIVED_ID, receiverUser.id);
-        message.put(Constants.KEY_MESSAGE, binding.inputMessage.getText().toString());
-        message.put(Constants.KEY_MESSAGE_IMAGE,false);
-        message.put(Constants.KEY_TIMESTAMP, new Date());
-        database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
-        if(conversionId != null)
+        if(!binding.inputMessage.getText().toString().equals(""))
         {
-            updateConversion(binding.inputMessage.getText().toString());
+            HashMap<String, Object> message = new HashMap<>();
+            message.put(Constants.KEY_SENDER_ID, preferenceManger.getString(Constants.KEY_USER_ID));
+            message.put(Constants.KEY_RECEIVED_ID, receiverUser.id);
+            message.put(Constants.KEY_MESSAGE, binding.inputMessage.getText().toString());
+            message.put(Constants.KEY_MESSAGE_IMAGE,false);
+            message.put(Constants.KEY_TIMESTAMP, new Date());
+
+            database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
+            if(conversionId != null)
+            {
+                updateConversion(binding.inputMessage.getText().toString());
+            }
+            else
+            {
+                HashMap<String, Object> conversion = new HashMap<>();
+                conversion.put(Constants.KEY_SENDER_ID, preferenceManger.getString(Constants.KEY_USER_ID));
+                conversion.put(Constants.KEY_SENDER_NAME, preferenceManger.getString(Constants.KEY_NAME));
+                conversion.put(Constants.KEY_SENDER_IMAGE, preferenceManger.getString(Constants.KEY_IMAGE));
+                conversion.put(Constants.KEY_RECEIVED_ID, receiverUser.id);
+                conversion.put(Constants.KEY_RECEIVER_NAME, receiverUser.name);
+                conversion.put(Constants.KEY_RECEIVER_IMAGE, receiverUser.image);
+                conversion.put(Constants.KEY_LAST_MESSAGE, binding.inputMessage.getText().toString());
+                conversion.put(Constants.KEY_TIMESTAMP, new Date());
+                addConversion(conversion);
+            }
         }
-        else
-        {
-            HashMap<String, Object> conversion = new HashMap<>();
-            conversion.put(Constants.KEY_SENDER_ID, preferenceManger.getString(Constants.KEY_USER_ID));
-            conversion.put(Constants.KEY_SENDER_NAME, preferenceManger.getString(Constants.KEY_NAME));
-            conversion.put(Constants.KEY_SENDER_IMAGE, preferenceManger.getString(Constants.KEY_IMAGE));
-            conversion.put(Constants.KEY_RECEIVED_ID, receiverUser.id);
-            conversion.put(Constants.KEY_RECEIVER_NAME, receiverUser.name);
-            conversion.put(Constants.KEY_RECEIVER_IMAGE, receiverUser.image);
-            conversion.put(Constants.KEY_LAST_MESSAGE, binding.inputMessage.getText().toString());
-            conversion.put(Constants.KEY_TIMESTAMP, new Date());
-            addConversion(conversion);
-        }
+
         binding.inputMessage.setText(null);
     }
 
